9f99ef264118957935c02788b78066c4213f3a1a
Giovanni Giansante <giannisante@gmail.com>
2013-05-31 04:04:48 -0300

    First cards commit

--- gen/com/example/suecaapp/R.java
+++ gen/com/example/suecaapp/R.java
@@ -11,16 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int plain_background=0x7f020002;
+        public static final int backcard=0x7f020000;
+        public static final int background=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int paus_01=0x7f020003;
+        public static final int paus_02=0x7f020004;
+        public static final int paus_03=0x7f020005;
+        public static final int paus_04=0x7f020006;
+        public static final int paus_05=0x7f020007;
+        public static final int paus_06=0x7f020008;
+        public static final int paus_07=0x7f020009;
+        public static final int paus_08=0x7f02000a;
+        public static final int paus_09=0x7f02000b;
+        public static final int paus_10=0x7f02000c;
+        public static final int paus_11=0x7f02000d;
+        public static final int paus_12=0x7f02000e;
+        public static final int paus_13=0x7f02000f;
+        public static final int plain_background=0x7f020010;
     }
     public static final class id {
         public static final int button1=0x7f070002;
+        public static final int carta=0x7f070007;
         public static final int creditos_menu=0x7f070003;
         public static final int menu_creditos=0x7f070001;
         public static final int menu_instrucoes=0x7f070000;
-        public static final int menu_settings=0x7f070007;
+        public static final int menu_settings=0x7f070008;
         public static final int textView1=0x7f070004;
         public static final int textView2=0x7f070005;
         public static final int textView3=0x7f070006;
@@ -29,6 +44,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int creditos=0x7f030001;
         public static final int instrucoes=0x7f030002;
+        public static final int jogo=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
--- src/com/example/suecaapp/MainActivity.java
+++ src/com/example/suecaapp/MainActivity.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 
 
-public class MainActivity extends Activity {
+
+
+public class MainActivity extends Activity  {
+
+	
+	
 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
 	}
+	
+	  public void chamaTela_Jogo(View v) {
+			setContentView(R.layout.jogo);
+					}
 	
 	  public void chamaTela_Creditos(View v) {
 			setContentView(R.layout.creditos);
@@ -29,12 +41,39 @@ public class MainActivity extends Activity {
 	}
 	
 
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+
+	   public void inicia_Jogo(View v) {
+		   
+			int face = 0;
+			
+			face = (int)(1 + Math.random() * 13);
+
+			        ImageView imgCarta = (ImageView) findViewById(R.id.carta);
+		    
+			        if (face==1) imgCarta.setImageResource(R.drawable.paus_01);
+					else if (face==2) imgCarta.setImageResource(R.drawable.paus_02);
+					else if (face==3) imgCarta.setImageResource(R.drawable.paus_03);
+					else if (face==4) imgCarta.setImageResource(R.drawable.paus_04);
+					else if (face==5) imgCarta.setImageResource(R.drawable.paus_05);
+					else if (face==6) imgCarta.setImageResource(R.drawable.paus_06);
+					else if (face==7) imgCarta.setImageResource(R.drawable.paus_07);
+					else if (face==8) imgCarta.setImageResource(R.drawable.paus_08);
+					else if (face==9) imgCarta.setImageResource(R.drawable.paus_09);
+					else if (face==10) imgCarta.setImageResource(R.drawable.paus_10);
+					else if (face==11) imgCarta.setImageResource(R.drawable.paus_11);
+					else if (face==12) imgCarta.setImageResource(R.drawable.paus_12);
+					else if (face==13) imgCarta.setImageResource(R.drawable.paus_13);
+		        
+			
+	   }
+		        
+
+	
 
 }
